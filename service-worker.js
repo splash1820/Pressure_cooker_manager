3b0e147466b9e9b44c84d953777ab1f3895aeae2
splash1820 <pranav.todkar@20gmail.com>
2025-07-13 11:40:20 +0530

    add pwa functionality

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'cooker-whistle-counter-v1';
+const urlsToCache = [
+  'whistle-counter.html',
+  'whistle-counter.js',
+  'manifest.json',
+  './favicon-16x16.png', // Make sure these paths are correct
+  './icon-32x32.png'  // relative to the service worker's scope
+];
+
+// Install event: cache all necessary assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event: serve from cache if available, otherwise fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
